773f331aa6e512bae45b50ed672d4d2f2b5737af
Ondrej Slamecka <ondrej@slamecka.cz>
2013-03-30 14:21:44 +0100

    Added valgrind test

--- run.sh
+++ run.sh
@@ -9,26 +9,40 @@ cp ../html.c ./
 
 
 # Go through all tests:
-NOT_PASSED=0
+DIFF_NOT_PASSED=0
+VALGRIND_NOT_PASSED=0
 for t in tests/* ; do
 	if [ -d "$t" ]; then
 		echo "-- Starting test $(basename $t) --"
 	
+		binary=./hw03_$(basename $t)
+        output=$(basename $t)_output.html
+
 		cat $t/main.c > main.c
-		gcc -std=c99 -Wall -Wextra -Werror domtree.h domtree.c html.h html.c main.c -o hw03
-		output=$(basename $t)_output.html
-		./hw03 < $t/input.html > $output
-		DIFF=$(diff $t/exp_output.html $output)
-		if [ "$DIFF" == "" ]; then
-			echo "Test passed"
+		gcc -std=c99 -Wall -Wextra -Werror domtree.h domtree.c html.h html.c main.c -o $binary
+
+        valgrind=$( { valgrind --leak-check=full -q $binary < $t/input.html > $output; } 2>&1 )
+		diff=$(diff $t/exp_output.html $output)
+
+		if [ "$diff" == "" ]; then
+			echo "Output comparison test passed"
 			rm $output
 		else
 			echo "Test $(basename $t) failed. Try running"
-			echo "	$(tput setaf 1)$ diff $t/exp_output.html $output$(tput setaf 7)"
-			NOT_PASSED=$((NOT_PASSED + 1))
+			echo "	$ $(tput setaf 1)diff $t/exp_output.html $output$(tput setaf 7)"
+			DIFF_NOT_PASSED=$((DIFF_NOT_PASSED + 1))
+		fi
+
+		if [ "$valgrind" == "" ]; then
+			echo "Valgrind test passed"
+			rm $binary
+		else
+			echo "Valgrind test failed. Try running"
+			echo "	$ $(tput setaf 1)valgrind $binary < $t/input.html$(tput setaf 7)"
+			VALGRIND_NOT_PASSED=$((VALGRIND_NOT_PASSED + 1))
 		fi
 	fi
 done
 
-echo "--- All tests ended. $NOT_PASSED failed. ---"
+echo "--- All tests ended. $DIFF_NOT_PASSED output comparisons failed. $VALGRIND_NOT_PASSED valgrind tests failed. ---"
 rm main.c
